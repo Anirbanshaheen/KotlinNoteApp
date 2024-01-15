6acaba9c77f8efe4cb01ab491de755f4fa4cc9eb
Anirbanshaheen <anirbanshaheen97@gmail.com>
2024-01-15 21:23:21 +0600

    Network generic class create

---
+++ app/src/main/java/com/example/kotlinnoteapp/utils/NetworkResult.kt
@@ -0,0 +1,8 @@
+package com.example.kotlinnoteapp.utils
+
+sealed class NetworkResult<T>(val data: T? = null, val message: String? = null) {
+
+    class SUCCESS<T>(data: T) : NetworkResult<T>(data)
+    class ERROR<T>(message: String?, data: T? = null) : NetworkResult<T>(data, message)
+    class LOADING<T> : NetworkResult<T>()
+}
